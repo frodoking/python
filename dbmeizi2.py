735ff7d1bcc9277c5af075ea92e612ef52f5d186
frodoking <awangyun8@gmail.com>
2015-01-06 23:27:20 +0800

    加入线程池

--- dbmeizi2.py
+++ dbmeizi2.py
@@ -1,14 +1,14 @@
 # -*- coding:utf-8 -*-
 import os
+import socket
 import urllib
 from HTMLParser import HTMLParser
 import re
-from threadPool import ThreadPool
-
-host_url = "http://www.dbmeizi.com/"
-root_dir = "D:\\work\\tmp\\dbmeizhi\\"
+import urllib2
+from thread_pool import WorkerManager
 
 
+# html页面解析
 class DBHtmlParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
@@ -34,6 +34,7 @@ class DBHtmlParser(HTMLParser):
             self.index += 1
 
 
+# 获取tab的url列表
 def get_tab_list(url):
     u = urllib.urlopen(url)
     html_content = u.read()
@@ -43,13 +44,15 @@ def get_tab_list(url):
     return m.tab_list
 
 
-def create_dir(dir_name):
-    path = os.path.join(root_dir, dir_name)
+# 创建不存在的目录
+def create_dir(dir, name):
+    path = os.path.join(dir, name)
     if not os.path.isdir(path):
         os.makedirs(path)
     return path
 
 
+# 获取当前tab中的图片url列表
 def get_image_url_list(image_url):
     u = urllib.urlopen(image_url)
     data = u.read()
@@ -58,36 +61,48 @@ def get_image_url_list(image_url):
     return image_url_list
 
 
+# 下载
 def down_image(image_url, image_dir, high):
     if high is True:  # 高清图片
         image_url = image_url.replace('s_', '')
-
-    print image_url
+    print image_url, image_dir, high
     try:
-        u = urllib.urlopen(image_url)
-        data = u.read()
+        fd = urllib2.urlopen(image_url)
+        data = fd.read()
         name = image_url[-13:-3]
-        f = open(image_dir + '\\' + '%s.jpg' % name, 'wb')
-        f.write(data)
-        f.close()
-    except Exception, e:
-        print e
+        f_hand = open(image_dir + '\\' + '%s.jpg' % name, 'wb')
+        f_hand.write(data)
+        f_hand.close()
+    except:
+        pass
 
-if __name__ == '__main__':
+
+def main():
+    host_url = "http://www.dbmeizi.com/"
+    root_dir = "D:\\work\\tmp\\dbmeizhi\\"
+
+    print '************ 开始 ***********'
     tab_list = get_tab_list(host_url)
-    # thread_pool = ThreadPool(len(tab_list))
+    # 使用线程池
+    socket.setdefaulttimeout(10)
+    wm = WorkerManager(len(tab_list))
     for tab in tab_list:
         tab_path = tab[0]
         tab_name = tab[1].decode('utf-8')
-        tab_dir = create_dir(tab_name)
+        tab_dir = create_dir(root_dir, tab_name)
 
         print '开始抓取 >>>> ', tab_path, tab_name, tab_dir
 
-        for p in range(10):
+        for p in range(10):  # 默认10页
             image_urls = get_image_url_list(host_url + '?p=%d' % p)
+            count = 0
             for url in set(image_urls):
+                count += 1
+                print '当前状态 >>>> ', tab_name, count, len(image_urls), ' <<<< '
                 down_image(url, tab_dir, True)
-                # thread_pool.add_job(down_image, url, tab_dir)
+                # wm.add_job(down_image, url, tab_dir, True)
+    wm.wait_for_complete()
+    print '************ 结束 ***********'
 
-    # thread_pool.wait_for_complete()
-    print '结束'
+if __name__ == '__main__':
+    main()
---
+++ thread_pool.py
@@ -0,0 +1,63 @@
+# -*- coding:utf-8 -*-
+import Queue
+import sys
+from threading import Thread
+
+
+# working thread
+class Worker(Thread):
+    worker_count = 0
+    timeout = 1
+
+    def __init__(self, work_queue, result_queue, **kwds):
+        Thread.__init__(self, **kwds)
+        self.id = Worker.worker_count
+        Worker.worker_count += 1
+        self.setDaemon(True)
+        self.workQueue = work_queue
+        self.resultQueue = result_queue
+        self.start()
+
+    def run(self):
+        """ the get-some-work, do-some-work main loop of worker threads """
+        while True:
+            try:
+                func, args, kwds = self.workQueue.get(timeout=Worker.timeout)
+                res = func(*args, **kwds)
+                print "worker[%2d]: %s" % (self.id, str(res))
+                self.resultQueue.put(res)
+                # time.sleep(Worker.sleep)
+            except Queue.Empty:
+                break
+            except:
+                print 'worker[%2d]' % self.id, sys.exc_info()[:2]
+                raise
+
+
+class WorkerManager:
+    def __init__(self, num_of_workers=10, timeout=5):
+        self.work_queue = Queue.Queue()
+        self.result_queue = Queue.Queue()
+        self.workers = []
+        self.timeout = timeout
+        self.recruit_threads(num_of_workers)
+
+    def recruit_threads(self, num_of_workers):
+        for i in range(num_of_workers):
+            worker = Worker(self.work_queue, self.result_queue)
+            self.workers.append(worker)
+
+    def wait_for_complete(self):
+        # ...then, wait for each of them to terminate:
+        while len(self.workers):
+            worker = self.workers.pop()
+            worker.join()
+            if worker.isAlive() and not self.work_queue.empty():
+                self.workers.append(worker)
+        print "All jobs are are completed."
+
+    def add_job(self, func, *args, **kwds):
+        self.work_queue.put((func, args, kwds))
+
+    def get_result(self, *args, **kwds):
+        return self.work_queue.get(*args, **kwds)
